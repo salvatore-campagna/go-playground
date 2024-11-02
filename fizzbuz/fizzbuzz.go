42badbf25700367561e26ed51937b978a4e33ac6
Salvatore Campagna <salvatore.campagna@elastic.co>
2024-11-02 13:30:56 +0100

    Add missing docs to FizzBuzz exercise

--- fizzbuz/fizzbuzz.go
+++ fizzbuz/fizzbuzz.go
@@ -1,3 +1,6 @@
+// Package fizzbuzz provides a function to generate a sequence of FizzBuzz values
+// up to a specified number. The classic FizzBuzz problem replaces multiples of 3
+// with "Fizz", multiples of 5 with "Buzz", and multiples of both with "FizzBuzz".
 package fizzbuzz
 
 import (
@@ -5,6 +8,13 @@ import (
 	"strings"
 )
 
+// FizzBuzz generates a FizzBuzz sequence up to the given number n.
+// For each integer from 1 to n:
+// - If the number is divisible by 3, "Fizz" is added to the output.
+// - If the number is divisible by 5, "Buzz" is added.
+// - If the number is divisible by both 3 and 5, "FizzBuzz" is added.
+// - Otherwise, the number itself is added as a string.
+// Each entry in the sequence is followed by a newline character.
 func FizzBuzz(n int) string {
 	var sb strings.Builder
 
