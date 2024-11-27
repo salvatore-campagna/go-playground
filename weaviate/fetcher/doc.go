48fe902850da6ce21e854843e963659f3a5a7032
Salvatore Campagna <salvatore.campagna@elastic.co>
2024-11-27 22:57:51 +0100

    fix: remove weaviate project

--- weaviate/fetcher/doc.go
+++
@@ -1,3 +0,0 @@
-// Package fetcher provides utilities for fetching and parsing term postings
-// from JSON data, sourced from either a URL or a local file.
-package fetcher
--- weaviate/go.mod
+++
@@ -1,3 +0,0 @@
-module weaviate
-
-go 1.23.2
